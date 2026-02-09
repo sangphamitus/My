997363e1c004d40ced5dbe2285f48dea46b65bd2
Sang Pham <phamphuocsang2834@gmail.com>
2026-02-09 23:36:36 +0700

    feat: add configuration for Tailwind CSS and utility functions, update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,4 +15,9 @@ export default defineConfig({
     tailwindcss(),
     // ...
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
